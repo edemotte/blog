22e79ad5841a03dbd51e243920b0a6eddfb50698
edemotte <eugenedelain@gmail.com>
2020-06-08 18:56:27 +0530

    created getPostIdParam to get the id of the post to use in the request later

--- app/js/post.js
+++ app/js/post.js
@@ -8,27 +8,13 @@ window.onload = () => {
 const getPostIdParam = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    return urlParams.get('id');
+    return urlParams.get("id");
 }
 
 const getPost = () => {
-    const url = `${API_URL}${getPostIdParam()}`
-    fetch(url, {
-        method: 'GET'
-    }).then((response)=>{
-        return response.json();
-    }).then((data)=>{
-        buildPost(data);
-    })
+    const postId = getPostIdParam();
 }
 
 const buildPost = (data) => {
-    console.log(data);
-    const postDate = new Date(parseInt(data.added_date)).toDateString();
-    const postImage = API_BASE_URL + data.post_image;
-    document.querySelector("header").style.backgroundImage = `url(${postImage})`;
-    document.getElementById("individual-post-title").innerText = data.title;
-    document.getElementById("individual-post-date").innerText =  `Published on ${postDate}`;
-    document.getElementById("individual-post-content").innerText = data.content;
-}
 
+}
